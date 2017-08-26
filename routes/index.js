117d2b8d7a4a16dcf6e5fca80d909a0a8ee9bb62
Mario Zamora <maza1387@gmail.com>
2017-08-26 12:13:16 -0700

    Authenticate User

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,32 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+// GET /login
+router.get('/login', function(req, res, next) {
+  return res.render('login', {title: 'Log In'});
+});
+
+// POST /login
+router.post('/login', function(req, res, next) {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+
+});
+
 // GET /register
 router.get('/register', function(req, res, next) {
   return res.render('register', {title: 'Sign Up'});
@@ -36,6 +62,7 @@ router.post('/register', function(req, res, next) {
         if (error) {
           return next(error);
         } else {
+          req.session.userId = user._id;
           return res.redirect('/profile');
         }
       })
